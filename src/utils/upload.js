39def116a0297c581f9eb3ae77b92a75c4a8633e
ASUS <you@example.com>
2025-04-15 17:30:03 +0200

    update code

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -1,23 +1,39 @@
 import axios from "axios";
 
+const toBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      const base64String = reader.result.split(",")[1]; 
+      resolve(base64String);
+    };
+    reader.onerror = (error) => reject(error);
+  });
+
 const upload = async (file) => {
   if (!file) {
     console.error("No file provided for upload.");
     return;
   }
 
-  const data = new FormData();
-  data.append("file", file);
-  data.append("upload_preset", "tupublish");
-  data.append("cloud_name", "dgpjnvlp3");
-
-  // const uploadUrl = import.meta.env.;
-  // console.log("Upload URL:", uploadUrl); // Debugging the upload URL
-
   try {
-    const res = await axios.post("https://api.cloudinary.com/v1_1/dgpjnvlp3/image/upload", data);
-    const { url } = res.data;
-    return url;
+    const base64Image = await toBase64(file);
+
+    const apiKey = "7df6d52cb9db492d1eba57dc97f8493e"; 
+
+    const body = new URLSearchParams();
+    body.append("key", apiKey);
+    body.append("image", base64Image);
+
+    const res = await axios.post("https://api.imgbb.com/1/upload", body, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    const imageUrl = res.data.data.url;
+    return imageUrl;
   } catch (err) {
     console.error("File upload error:", err.response?.data || err.message);
     throw err;
